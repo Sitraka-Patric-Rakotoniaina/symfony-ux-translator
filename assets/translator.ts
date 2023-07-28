d3f263350ae415e98a3cd58bf7c545e2cd09ea88
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-07-28 13:03:48 +0200

    Remove code for tutorial

--- assets/translator.ts
+++ assets/translator.ts
@@ -3,12 +3,6 @@ import { trans, setLocaleFallbacks } from '@symfony/ux-translator';
 
 setLocaleFallbacks(localeFallbacks);
 
-export function getLocale(): string {
-    const locale: HTMLDivElement | null = document.querySelector('#locale');
-
-    return locale ? locale.dataset['locale'] : 'en';
-}
-
 export { trans };
 
 export * from '../var/translations';
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Translation\LocaleSwitcher;
 
 class HomeController extends AbstractController
 {
-    #[Route('/{locale?en}', name: 'home_index', requirements: ['locale' => 'en|fr'])]
-    public function index(LocaleSwitcher $localeSwitcher, string $locale): Response
+    #[Route('/', name: 'home_index')]
+    public function index(): Response
     {
-        $localeSwitcher->setLocale($locale);
-
         return $this->render('home/index.html.twig');
     }
 }
