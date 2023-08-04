64b6732437009c504ca07f0a1479572e7124bc25
Sitraka <sitrakiniainaptc@gmail.com>
2023-08-04 09:23:14 +0300

    interactive translator

--- assets/controllers/translator-controller.ts
+++ assets/controllers/translator-controller.ts
@@ -1,6 +1,23 @@
 import { Controller } from '@hotwired/stimulus';
+import {LABELS_JOB_TITLE, trans} from "../translator";
 
 export default class extends Controller<HTMLDivElement> {
+
+    static targets: string[] = [
+        "jobTitleOutput",
+        "jobTitleInput"
+    ];
+
+    declare readonly jobTitleOutputTarget: HTMLSpanElement;
+    private readonly jobTitleInputTarget: HTMLInputElement;
+
     connect(): void {
+        this.render();
+    }
+
+    render () {
+        this.jobTitleOutputTarget.textContent = trans(LABELS_JOB_TITLE, {
+            name: this.jobTitleInputTarget.value
+        });
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home_index')]
-    public function index(): Response
+    #[Route('/{locale?en}', name: 'home_index', requirements: ['locale' => 'en|fr'])]
+    public function index(TranslatorInterface $translator, LocaleSwitcher $switcher, string $locale): Response
     {
+        $switcher->setLocale($locale);
         return $this->render('home/index.html.twig');
     }
 }
