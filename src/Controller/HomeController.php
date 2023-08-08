f3d1e983a56dc230af3ecf7911c1a65d07616c92
Sitraka <sitrakiniainaptc@gmail.com>
2023-08-08 14:44:11 +0300

    debug sh script executing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Translation\LocaleSwitcher;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
     #[Route('/{locale?en}', name: 'home_index', requirements: ['locale' => 'en|fr'])]
-    public function index(TranslatorInterface $translator, LocaleSwitcher $switcher, string $locale): Response
+    public function index(LocaleSwitcher $switcher, string $locale): Response
+    {
+        $users = $this->userRepository->findAll();
+        $switcher->setLocale($locale);
+        return $this->render('home/index.html.twig', ['users' => $users]);
+    }
+
+    #[Route('/create_file/{locale?en}', name: 'create_file', requirements: ['locale' => 'en|fr'])]
+    public function createFIle(LocaleSwitcher $switcher, string $locale): Response
     {
         $switcher->setLocale($locale);
-        return $this->render('home/index.html.twig');
+        $process = new Process(['sh', realpath(__DIR__ . '/../../script/test.sh')]);
+        $process->run();
+        if ($process->isSuccessful()) {
+            dd($process->getOutput());
+        } else {
+            dd('error' . $process->getErrorOutput());
+        }
+        return $this->redirectToRoute('home_index', ['locale' => $locale]);
     }
 }
---
+++ src/script/test.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Hello, monde! >> test.txt
+echo Success command
